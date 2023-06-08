7d5aa132296f2e11382b9270762e9085fc95e046
Juan <111772146+Juan-Severiano@users.noreply.github.com>
2023-06-08 00:01:43 +0000

    editor config e eslint instaladdos

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  parse: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
